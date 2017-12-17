6d2fc23388553d552a7abfde383db90280de73ee
zhiguodong <xiaodong0525@163.com>
2017-12-17 22:48:34 +0800

    studay webpack

---
+++ src/index.js
@@ -0,0 +1,23 @@
+
+import _ from 'lodash';
+import './style.css';
+import printMe from './print.js';
+
+function component(){
+	
+	var element  = document.createElement('div');
+	var btn = document.createElement('button');
+	
+	// Loash, currently included via a script, is required for this line to work
+	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+	element.classList.add('hello');
+	
+	btn.innerHTML = 'Click me and check the console!';
+	btn.onclick = printMe;
+	
+	element.appendChild(btn);
+	
+	return element;
+}
+
+document.body.appendChild( component());
